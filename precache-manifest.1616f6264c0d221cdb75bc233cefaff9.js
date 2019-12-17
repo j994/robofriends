3f8954af5ad74ddeffc97ac85869f02e620e23c2
Jeff Landers <jeff.landers12@gmail.com>
2019-12-17 12:47:33 +0100

    Updates

---
+++ precache-manifest.1616f6264c0d221cdb75bc233cefaff9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f04d1eee28aa756e7e276dea0e55d64d",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "138003460e81a5e84e43",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "e054a0875d125e48f758",
+    "url": "/robofriends/static/css/main.60bc22d3.chunk.css"
+  },
+  {
+    "revision": "138003460e81a5e84e43",
+    "url": "/robofriends/static/js/2.38e71f33.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robofriends/static/js/2.38e71f33.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e054a0875d125e48f758",
+    "url": "/robofriends/static/js/main.c8823c62.chunk.js"
+  },
+  {
+    "revision": "1e6fdd492adfb4acd267",
+    "url": "/robofriends/static/js/runtime-main.2c4b189a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
